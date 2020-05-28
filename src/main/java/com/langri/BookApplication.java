28afb2f44508a8e4588758c9961f18f855f64247
LangRi <18510444551@163.com>
2020-05-28 18:19:29 +0800

    修改配置文件，修改父工程

--- src/main/java/com/langri/BookApplication.java
+++ src/main/java/com/langri/BookApplication.java
@@ -1,17 +1,15 @@
 package com.langri;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author LangRi
  * 2020/5/24 10:33
  * @version 1.0.0
  */
-@SpringBootApplication
+/*@SpringBootApplication
 //@EnableAdminServer
 public class BookApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
     }
-}
+}*/
