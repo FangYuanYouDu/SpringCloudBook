02bb116035339e32a98f3be651b6b3878abd5dc5
LangRi <18510444551@163.com>
2020-06-02 15:28:24 +0800

    修改user-service配置文件

--- user-service/src/main/java/com/langri/UserServiceApplication.java
+++ user-service/src/main/java/com/langri/UserServiceApplication.java
@@ -2,12 +2,15 @@ package com.langri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author FangYuan
  * 2020/5/29 14:04
  * @version 1.0.0
  */
+@EnableDiscoveryClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
